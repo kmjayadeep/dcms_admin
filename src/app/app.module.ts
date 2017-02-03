0940f903a32233fe69839fa21680b6800a386ec1
Jayadeep KM <kmjayadeep@gmail.com>
2017-02-03 20:05:08 +0530

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { APP_BASE_HREF } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
+import { routes } from './app.routes';
+
+import { LoginModule } from './login/login.module';
+import { SignupModule } from './signup/signup.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	imports: [
+		BrowserModule,
+		HttpModule,
+		RouterModule.forRoot(routes),
+		LoginModule,
+		SignupModule,
+		DashboardModule,
+		SharedModule.forRoot()
+	],
+	declarations: [AppComponent],
+	providers: [{
+		provide: APP_BASE_HREF,
+		useValue: '<%= APP_BASE %>'
+	}],
+	bootstrap: [AppComponent]
+
 })
+
 export class AppModule { }
