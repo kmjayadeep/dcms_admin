73248c925255c8c011138d2fbbb88dc129dbebfc
Nisham <moonblade168@gmail.com>
2017-03-14 17:56:51 +0530

    added group count info, needs css work

--- src/app/dashboard/event-registration/event-registration.component.ts
+++ src/app/dashboard/event-registration/event-registration.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { DomSanitizer, SafeHtml } from "@angular/platform-browser";
-import {EventService} from '../../services/event/event.service'
-import {UserService} from '../../services/user.service'
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import {EventService} from '../../services/event/event.service';
+import {UserService} from '../../services/user.service';
 import { DataTableResource } from 'angular-2-data-table-bootstrap4';
 
 @Component({
@@ -16,62 +16,60 @@ export class EventRegistrationComponent implements OnInit {
   event = null;
   registeredStudents = [];
   eventList = [];
-  selectedEvent=null;
-  constructor(private eventService:EventService, private _sanitizer: DomSanitizer) {		
+  selectedEvent= null;
+  constructor(private eventService: EventService, private _sanitizer: DomSanitizer) {
  }
 
   ngOnInit() {
     this.getEvents();
-    this.error = "Please wait for loading events (this message will be gone when loaded)";
+    this.error = 'Please wait for loading events (this message will be gone when loaded)';
   }
-	
+
   getEvents(){
     this.eventList = [];
-		this.error = null;
-		this.message = null;
-		this.event = null;
-		this.eventService.getEventAndWorkShop()
-		.then((events:any)=>{
-      this.error=null;
-			console.log(events)
-      this.eventList = events.map(x=>{
+    this.error = null;
+    this.message = null;
+    this.event = null;
+    this.eventService.getEventAndWorkShop()
+    .then((events: any) => {
+      this.error = null;
+      console.log(events);
+      this.eventList = events.map(x => {
         return {
           id: x.id,
           name: x.name,
           group: x.group
         };
       });
-		})
-		.catch(err=>{
-			console.log(err)
-			this.error = 'Unable to load Event list'
-		})
-	}
+    })
+    .catch(err => {
+      console.log(err);
+      this.error = 'Unable to load Event list';
+    });
+  }
 
     autocompleListFormatter = (data: any) : SafeHtml => {
-    let html = `<span>${data.name}</span>`;
+    const html = `<span>${data.name}</span>`;
     return this._sanitizer.bypassSecurityTrustHtml(html);
   }
 
   getRegistered(){
-		this.error = null;
+    this.error = 'Loading, please wait';
     this.registeredStudents = [];
     console.log(this.selectedEvent);
-    this.eventService.getRegisteredStudents(this.selectedEvent.id)
-    .then(result=>{
+    this.eventService.getRegisteredCount(this.selectedEvent.id)
+    .then(result => {
       console.log(result);
-      if(result.length>0)
-      {
-        this.event=result[0];
-        this.registeredStudents = this.event.students;
-        if (this.registeredStudents.length==0)
-          this.error="No student registrations yet";
-      }else{
-        this.error="Unable to get event";
-      }
-    }).catch(error=>{
+        this.error = null;
+        this.event = result;
+        if (result.students.length === 0){
+          this.error = 'No student registrations yet';
+        } else {
+          this.registeredStudents = result.students;
+        }
+    }).catch(error => {
       console.log(error);
-      this.error = 'Unable to get students'
-    })
+      this.error = 'Unable to get students';
+    });
   }
 }
--- src/app/services/event/event.service.ts
+++ src/app/services/event/event.service.ts
@@ -12,101 +12,111 @@ import {AngularFire, FirebaseApp,AuthProviders,AuthMethods} from 'angularfire2';
 export class EventService extends ConnectionService{
 
 
-	private eventApi = this.serverUrl + '/event';
+    private eventApi = this.serverUrl + '/event';
 
-	constructor(public af: AngularFire, @Inject(FirebaseApp) private firebaseApp: any, protected http: Http) {
-		super(af,firebaseApp,http);
-	}
+    constructor(public af: AngularFire, @Inject(FirebaseApp) private firebaseApp: any, protected http: Http) {
+        super(af,firebaseApp,http);
+    }
 
-	getEvents(){
-		return this.getHeaders()
-		.then(headers=>{
-			return this.http
-			.get(this.eventApi,{
-				headers:headers
-			})
-			.map(res=>res.json())
-			.toPromise()
-		})
-	}
+    getEvents(){
+        return this.getHeaders()
+        .then(headers=>{
+            return this.http
+            .get(this.eventApi,{
+                headers:headers
+            })
+            .map(res=>res.json())
+            .toPromise()
+        })
+    }
 
-	getEvent(eventId){
-		return this.getHeaders()
-		.then(headers=>{
-			return this.http
-			.get(this.eventApi+'/'+eventId,{
-				headers:headers
-			})
-			.map(res=>res.json())
-			.toPromise()
-		})
-	}
+    getEvent(eventId){
+        return this.getHeaders()
+        .then(headers=>{
+            return this.http
+            .get(this.eventApi+'/'+eventId,{
+                headers:headers
+            })
+            .map(res=>res.json())
+            .toPromise()
+        })
+    }
 
-	deleteEvent(eventId){
-		return this.getHeaders()
-		.then(headers=>{
-			return this.http
-			.delete(this.eventApi+'/'+eventId,{
-				headers:headers
-			})
-			.map(res=>res.json())
-			.toPromise()
-		})
-	}
+    deleteEvent(eventId){
+        return this.getHeaders()
+        .then(headers=>{
+            return this.http
+            .delete(this.eventApi+'/'+eventId,{
+                headers:headers
+            })
+            .map(res=>res.json())
+            .toPromise()
+        })
+    }
 
-	updateEvent(event){
-		return this.getHeaders()
-		.then(headers=>{
-			return this.http
-			.post(this.eventApi+'/'+event.id,event,{
-				headers:headers
-			})
-			.map(res=>res.json())
-			.toPromise()
-		})
-	}
+    updateEvent(event){
+        return this.getHeaders()
+        .then(headers=>{
+            return this.http
+            .post(this.eventApi+'/'+event.id,event,{
+                headers:headers
+            })
+            .map(res=>res.json())
+            .toPromise()
+        })
+    }
 
-	addEvent(event){
-		return this.getHeaders()
-		.then(headers=>{
-			return this.http
-			.put(this.eventApi,event,{
-				headers:headers
-			})
-			.map(res=>res.json())
-			.toPromise()
-		})
-	}
+    addEvent(event){
+        return this.getHeaders()
+        .then(headers=>{
+            return this.http
+            .put(this.eventApi,event,{
+                headers:headers
+            })
+            .map(res=>res.json())
+            .toPromise()
+        })
+    }
 
-	uploadPic(eventId,file){
-		let path = 'images/event/'+eventId+file.name
-		let ref = this.firebaseApp.storage().ref().child(path)
-		return ref
-				.put(file)
-				.then(res=>{
-					return new Promise((resolve,reject)=>{
-						ref.getDownloadURL().then(url => resolve(url))
-							.catch(err=>reject(err))
-					})
-				})
-	}
+    uploadPic(eventId,file){
+        let path = 'images/event/'+eventId+file.name
+        let ref = this.firebaseApp.storage().ref().child(path)
+        return ref
+                .put(file)
+                .then(res=>{
+                    return new Promise((resolve,reject)=>{
+                        ref.getDownloadURL().then(url => resolve(url))
+                            .catch(err=>reject(err))
+                    })
+                })
+    }
 
-	getRegisteredStudents(eventId){
-		return this.getHeaders()
-		.then(headers=>{
-			return this.http
-			.get(this.eventApi+'/student/'+eventId,{
-				headers: headers
-			}).map(res=>res.json())
-			.toPromise();
-		})
-	}
+    getRegisteredStudents(eventId){
+        return this.getHeaders()
+        .then(headers=>{
+            return this.http
+            .get(this.eventApi+'/student/'+eventId,{
+                headers: headers
+            }).map(res=>res.json())
+            .toPromise();
+        })
+    }
 
-	getEventAndWorkShop(){
-		return this.http
-		.get(this.serverUrl.split('/').slice(0,-1).join('/')+'/public/event',{})
-		.map(res=>res.json())
-		.toPromise();
-	}
-	
+    getRegisteredCount(eventId) {
+        return this.getHeaders()
+        .then(headers=>{
+            return this.http
+            .get(this.eventApi+'/registeredCount/'+eventId,{
+                headers: headers
+            }).map(res=>res.json())
+            .toPromise();
+        })
+    }
+    getEventAndWorkShop(){
+        return this.http
+        .get(this.serverUrl.split('/').slice(0,-1).join('/')+'/public/event',{})
+        .map(res=>res.json())
+        .toPromise();
+    }
+    
 }
